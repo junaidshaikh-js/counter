2b0cafd2331ce0d53b5439749fc153dd0c2f50cb
Junaid <sj.shaikhjunaid@gmail.com>
2021-06-08 16:41:11 +0530

    update files

--- js/index.js
+++ js/index.js
@@ -3,15 +3,31 @@ const countNumber = document.querySelector(".count-number");
 const btnContainer = document.querySelector(".btn-container");
 
 // increase function
-const increase = () => +countNumber.innerHTML++;
+const increase = () => {
+  // unary + operator convert the string into number
+  +countNumber.innerHTML++;
+
+  // change color if number is greater than 0
+  if (+countNumber.innerHTML > 0) {
+    countNumber.style.color = "green";
+  }
+};
 
 // decrease function
-const decrease = () => +countNumber.innerHTML--;
+const decrease = () => {
+  +countNumber.innerHTML--;
+  if (+countNumber.innerHTML < 0) {
+    countNumber.style.color = "red";
+  }
+};
 
 // reset function
-const reset = () => (countNumber.innerHTML = 0);
+const reset = () => {
+  countNumber.innerHTML = 0;
+  countNumber.style.color = "#102a42";
+};
 
-// handle operation
+// handle operations
 function count(e) {
   let id = e.target.id;
   switch (id) {
